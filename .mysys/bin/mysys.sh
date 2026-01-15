d04d1bc40fc84547740baaa53a2c117f9ad47264
joao tiago viegas <3536754+jtviegas@users.noreply.github.com>
2026-01-16 00:13:52 +0100

    added more stuff

--- .mysys/bin/mysys.sh
+++ .mysys/bin/mysys.sh
@@ -33,11 +33,20 @@ err(){
 }
 
 # ---------- CONSTANTS ----------
+export SYS_VARIABLES=${SYS_VARIABLES:-".sys_variables"}
 export FILE_VARIABLES=${FILE_VARIABLES:-".variables"}
 export FILE_SECRETS=${FILE_SECRETS:-".secrets"}
 export TAR_FILE="mysys.tar.bz2"
-export CLAUDE_FOLDER="$HOME/.claude"
+
 # -------------------------------
+if [ ! -f "$env_folder/$SYS_VARIABLES" ]; then
+  warn "we DON'T have a $SYS_VARIABLES variables file - creating it"
+  touch "$env_folder/$SYS_VARIABLES"
+else
+   # shellcheck disable=SC1090
+  . "$env_folder/$SYS_VARIABLES"
+fi
+
 if [ ! -f "$env_folder/$FILE_VARIABLES" ]; then
   warn "we DON'T have a $FILE_VARIABLES variables file - creating it"
   touch "$env_folder/$FILE_VARIABLES"
@@ -74,16 +83,151 @@ update(){
   rm $TAR_FILE
 
 if [ -d "$CLAUDE_FOLDER" ] ; then
-    [ ! -d "$CLAUDE_FOLDER/skills" ] && mkdir -p "$CLAUDE_FOLDER/skills"
-    cd "$CLAUDE_FOLDER/skills" && ln -sf "$mysys_folder/agent_docs/skills/coder" "coder"
-    info "[update] coder skill added to claude"
-    cd "$mysys_folder" || exit 1
-  fi  
+  info "[update] claude is here"
+  [ ! -d "$CLAUDE_FOLDER/skills" ] && mkdir -p "$CLAUDE_FOLDER/skills"
+  cd "$CLAUDE_FOLDER/skills" && ln -sf "$mysys_folder/agent_docs/skills/coder" "coder"
+  info "[update] coder skill added to claude"
+fi  
 
   cd "$_pwd" || exit 1
   echo "[update] ...done."
 }
 
+
+sys_basic_reqs_macos(){
+  info "[sys_basic_reqs_macos|in]"
+  
+  [ -z "$BASIC_REQS_MACOS" ] && err "[sys_basic_reqs_macos|out] must provide BASIC_REQS_MACOS env var" && usage
+  local result=0
+  for app in $BASIC_REQS_MACOS; do
+    info "[sys_basic_reqs_macos] adding: $app"
+    sudo apt install "$app"
+    result="$?"
+    [ ! "$result" -eq "0" ] && err "[sys_basic_reqs_macos] could not install: $app" && exit 1
+	done
+  
+  local msg="[sys_basic_reqs_macos|out] => ${result}"
+  [ ! "$result" -eq "0" ] && err "$msg" && exit 1
+  info "$msg"
+}
+
+sys_basic_reqs_linux(){
+  info "[sys_basic_reqs_linux|in]"
+  
+  [ -z "$BASIC_REQS_LINUX" ] && err "[sys_basic_reqs_linux|out] must provide BASIC_REQS_LINUX env var" && usage
+  local result=0
+  for app in $BASIC_REQS_LINUX; do
+    info "[sys_basic_reqs_linux] adding: $app"
+    sudo apt install "$app"
+    result="$?"
+    [ ! "$result" -eq "0" ] && err "[sys_basic_reqs_linux] could not install: $app" && exit 1
+	done
+  
+  local msg="[sys_basic_reqs_linux|out] => ${result}"
+  [ ! "$result" -eq "0" ] && err "$msg" && exit 1
+  info "$msg"
+}
+
+sys_basic_reqs(){
+  info "[sys_basic_reqs|in]"
+  
+  local osname=$(uname)
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+  	info "[sys_basic_reqs] running in MACOS"
+  	sys_basic_reqs_macos
+  elif [ "$osname" == "Linux" ]; then
+  	info "[sys_basic_reqs] running in LINUX"
+  	sys_basic_reqs_linux
+  else
+  	err "[sys_basic_reqs|out] can't support this OS" && exit 1
+  fi
+  result="$?"
+  local msg="[sys_basic_reqs|out] => ${result}"
+  [ ! "$result" -eq "0" ] && err "$msg" && exit 1
+  info "$msg"
+}
+
+sys_reqs(){
+  info "[sys_reqs|in] ($1)"
+
+   local osname=$(uname)
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+  	info "[sys_reqs] running in MACOS"
+  	sys_reqs_macos
+  elif [ "$osname" == "Linux" ]; then
+  	info "[sys_reqs] running in LINUX"
+  	sys_reqs_linux
+  else
+  	err "[sys_reqs|out] can't support this OS" && exit 1
+  fi
+  result="$?"
+  local msg="[sys_reqs|out] => ${result}"
+  [ ! "$result" -eq "0" ] && err "$msg" && exit 1
+  info "$msg"
+}
+
+sys_reqs_linux(){
+  info "[sys_reqs_linux|in]"
+  
+  [ -z "$SYS_REQS_LINUX" ] && err "[sys_reqs_linux|out] must provide SYS_REQS_LINUX env var" && usage
+  
+  local result=0
+  info "[sys_reqs_linux] adding: vscode"
+	sudo snap install --classic code
+	result="$?"
+  [ ! "$result" -eq "0" ] && err "[sys_reqs_linux|out] could not install: vscode" && exit 1
+
+  for app in $SYS_REQS_LINUX; do
+    info "[sys_reqs_linux] adding: $app"
+    [ "$app" == "code" ] && continue
+    sudo apt install "$app" 
+    result="$?"
+    [ ! "$result" -eq "0" ] && err "[sys_reqs_linux|out] could not install: $app" && exit 1
+	done
+  
+  local msg="[sys_reqs_linux|out] => ${result}"
+  [ ! "$result" -eq "0" ] && err "$msg" && exit 1
+  info "$msg"
+}
+
+sys_reqs_macos(){
+  info "[sys_reqs_macos|in]"
+  
+  [ -z "$SYS_REQS_MACOS" ] && err "[sys_reqs_macos|out] must provide SYS_REQS_MACOS env var" && usage
+  local result=0
+  for app in $SYS_REQS_MACOS; do
+    info "[sys_reqs_macos] adding: $app"
+    sudo apt install "$app"
+    result="$?"
+    [ ! "$result" -eq "0" ] && err "[sys_reqs_macos|out] could not install: $app" && exit 1
+	done
+  
+  local msg="[sys_reqs_macos|out] => ${result}"
+  [ ! "$result" -eq "0" ] && err "$msg" && exit 1
+  info "$msg"
+}
+
+
+ssh_default_key(){
+  info "[ssh_default_key|in]"
+
+	local key="id_rsa"
+  local result=0
+  if [ ! -f "~/.ssh/${key}" ]; then
+  	info "creating new ssh key: ${key}"
+  	ssh-keygen -t rsa -b 4096 -C "jtviegas@gmail.com" && \
+  		eval "$(ssh-agent -s)" && \
+  		ssh-add ~/.ssh/${key}
+  	 result="$?"
+  fi 
+
+  local msg="[ssh_default_key|out] => ${result}"
+  [ ! "$result" -eq "0" ] && err "$msg" && exit 1
+  info "$msg"
+}
+
+# ---------- BE SURE TO HAVE BASIC REQS ----------
+sys_basic_reqs
 
 # -------------------------------------
 usage() {
@@ -95,7 +239,10 @@ usage() {
   $(basename "$0") { command }
 
     commands:
-      - update: updates 'mysys'
+      - update:               updates 'mysys'
+      - sys_basic_reqs				installs basic sys requirements
+      - sys_reqs							installs other sys requirements
+      - ssh_default_key       creates a default ssh key if none exists
 EOM
   exit 1
 }
@@ -105,6 +252,15 @@ debug "1: $1 2: $2 3: $3 4: $4 5: $5 6: $6 7: $7 8: $8 9: $9"
 case "$1" in
   update)
     update
+    ;;
+  sys_basic_reqs)
+    sys_basic_reqs
+    ;;
+  sys_reqs)
+    sys_reqs
+    ;;
+  ssh_default_key)
+    ssh_default_key
     ;;
   *)
     usage
