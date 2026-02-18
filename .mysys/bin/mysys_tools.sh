1a3f4a8d548a332c1aa57b3d828f9a765956a309
joao tiago viegas <3536754+jtviegas@users.noreply.github.com>
2026-02-19 00:50:18 +0100

    latest

--- .mysys/bin/mysys_tools.sh
+++ .mysys/bin/mysys_tools.sh
@@ -112,6 +112,8 @@ EOF
 
     sudo apt update
     sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo groupadd docker
+    sudo usermod -aG docker "$USER"
 
   fi
 
