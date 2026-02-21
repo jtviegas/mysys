3465a788d90b27e444f0151b974f155dbbff567b
joao tiago viegas <3536754+jtviegas@users.noreply.github.com>
2026-02-21 11:34:14 +0100

    wip

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,7 @@ _pwd=$(pwd)
 
 [ ! -d "$MYSYS_FOLDER" ] && mkdir -p "$MYSYS_FOLDER/bin"
 cd "$MYSYS_FOLDER/bin" || cd "$_pwd" && exit 1
-wget https://raw.githubusercontent.com/jtviegas/mysys/main/.mysys/bin/mysys.sh && \
+curl -O https://raw.githubusercontent.com/jtviegas/mysys/main/.mysys/bin/mysys.sh && \
   chmod +x mysys.sh && \
   ./mysys.sh update
 
