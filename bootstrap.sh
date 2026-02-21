c026e7dea509b9e26e37d083d72d222483868dee
joao tiago viegas <3536754+jtviegas@users.noreply.github.com>
2026-02-21 11:10:04 +0100

    wip

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,23 +1,8 @@
 #!/usr/bin/env bash
-
 # http://bash.cumulonim.biz/NullGlob.html
 shopt -s nullglob
-# -------------------------------
-this_folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-if [ -z "$this_folder" ]; then
-  this_folder=$(dirname "$(readlink -f "$0")")
-fi
-mysys_folder=$(dirname "$this_folder")
-# shellcheck disable=SC2034
-env_folder="$mysys_folder/env"
-# -------------------------------
-# shellcheck disable=SC1091
-. "$this_folder/.include.sh"
-
 # ---------- CONSTANTS ----------
-
 export MYSYS_FOLDER="$HOME/.mysys"
-
 # ---------- main ----------
 info "[bootstrap|in]"
 
